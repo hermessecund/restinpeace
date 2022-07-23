f850d73cdf6b981b845bc8ac1f81eae6f217fd8a
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-23 22:30:34 +0100

    Update Country.js

--- src/components/Country.js
+++ src/components/Country.js
@@ -9,7 +9,7 @@ const Country = () => {
   useEffect(() => {
     const fetchCountryData = async () => {
       const response = await fetch(
-        `https://restcountries.com/v2/name/{name}`
+        `https://restcountries.com/v2/name/${name}`
       )
       const country = await response.json()
       setCountry(country)
